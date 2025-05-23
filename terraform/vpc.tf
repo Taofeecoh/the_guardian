f1efded565d4cbac291a9846c17246b4f50d45fe
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-05-23 14:21:16 +0100

    updates vpc aattributes

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,9 +2,9 @@ resource "aws_vpc" "theguardian" {
   cidr_block = "10.0.0.0/16"
   
   tags = {
-    name = "the-guardian"
-    environment = "development"
-    team = "engineering"
-    used_for = "everything_nigerian_news"
+    Name = "the-guardian"
+    Environment = "development"
+    Team = "engineering"
+    Used_for = "everything_nigerian_news"
  }
 }
