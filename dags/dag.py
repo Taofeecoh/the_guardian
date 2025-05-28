1eb4f94fdb3f0bbf97cdacfa69986031b4763c08
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-05-28 11:27:19 +0100

    update dag

--- dags/dag.py
+++ dags/dag.py
@@ -2,14 +2,28 @@ from datetime import datetime, timedelta
 
 import pipeline as p
 from airflow import DAG
-from airflow.models import Variable
 from airflow.operators.python import PythonOperator
+
+
+default_args = {
+    'owner': 'taofeecoh',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 5, 27),
+    'email': ['adesanutaofeecoh@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'schedule_interval': '30 6,18,23 * * *',
+}
+
 
 dag = DAG(
     dag_id='the-guardian',
-    description='the guardian pipeline',
-    start_date=datetime.today(),
-    schedule='18 17 * * *'
+    description='dag that runs a pipeline at 6:30am, 6:30pm, and 11:30pm',
+    default_args=default_args
+    #schedule_interval='30 6,18,23 * * *',
+    #dagrun_timeout=timedelta(minutes=5.0)
 )
 
 extract_data = PythonOperator(
