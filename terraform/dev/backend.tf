92d3160b47b7626f1a126df75a1dffc7289e36fe
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-05-30 02:36:40 +0100

    updated terraform backend

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-theguardian"
+    bucket = "terraformbackend-theguardian"
     key    = "dev/terraform.tfstate"
     region = "eu-central-1"
+    use_lockfile = true
   }
 }
